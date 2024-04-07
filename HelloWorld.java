9d324eba3d200547993c87b40c015fdac646d60d
ben270lim <166254846+ben270lim@users.noreply.github.com>
2024-04-06 21:46:50 -0400

    Update HelloWorld.java
    Adds a print message to the code that prints "My name is Benjamin Lim". 
    
    -Benjamin Lim

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.print("Benjamin Lim");
+    System.out.print("My name is Benjamin Lim");
   }
 }
